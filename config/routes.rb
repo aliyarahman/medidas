997c166c991adca1cacd32c3d521890ecbbe55ff
aliyarahman <aliya.rahman@yahoo.com>
2014-07-20 21:04:33 -0400

    Converted to named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   match '/help', 	to: 'static_pages#help', 	via: 'get'
   match '/about', 	to: 'static_pages#about',	via: 'get'
   match '/contact',	to: 'static_pages#contact', 	via: 'get'
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
