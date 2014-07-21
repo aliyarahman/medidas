3ddffdd78e8cb79b87da7e402ead8ef23ade623b
aliyarahman <aliya.rahman@yahoo.com>
2014-07-20 20:42:07 -0400

    Updated route tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
   describe "Home page" do
 
     it "should have the content 'Medidas'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Medidas')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Medidas")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
@@ -27,12 +27,12 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-	visit '/static_pages/help'
+	visit help_path
 	  expect(page).to have_content('Help')
     end#{base_title} 
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")  
     end
   end
@@ -40,12 +40,12 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About'" do
-        visit '/static_pages/about'
+        visit about_path
           expect(page).to have_content('About')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
@@ -53,12 +53,12 @@ describe "StaticPages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-        visit '/static_pages/contact'
+        visit contact_path
           expect(page).to have_content('Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
