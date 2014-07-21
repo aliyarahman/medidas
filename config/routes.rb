92499d94a38c086a237040cb2b6734ede8ffae22
aliyarahman <aliya.rahman@yahoo.com>
2014-07-20 20:47:04 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
+  match '/help', 	to: 'static_pages#help', 	via: 'get'
+  match '/about', 	to: 'static_pages#about',	via: 'get'
+  match '/contact',	to: 'static_pages#contact', 	via: 'get'
   resources :hours_entries
 
   resources :users
